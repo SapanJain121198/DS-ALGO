f9ead01a59352b2bffae8e9c58720290312527c9
Sapan Jain <Sapan.Jain@viacomcontractor.com>
2021-04-19 09:29:55 +0530

    commit

---
+++ DS ALGO/src/BitManipulation/DivideTwoIntegersWithoutDivisionMultiplicationMod.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+public class DivideTwoIntegersWithoutDivisionMultiplicationMod {
+	
+//	when we divide 10011 with 2 then remainder is 1 and quotient is 1001 , with 4 then 11 and quotient is 100 , with 8 then 011 and quotient is 10 with 16 then 0011 and quotient is 1, with 32 then 10011 and quotient is 0
+
+	
+// n << x = n * pow(2,x)
+}
